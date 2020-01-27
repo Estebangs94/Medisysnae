a5c0a9b744e43d5b59c181a2fe7c55f343cdb947
Esteban <estebangs9@gmail.com>
2020-01-27 01:16:42 -0300

    Cambio de enfoque para Turnos. Falta OnPost en create y modificar listado de turnos.

--- Medisysnae/Medisysnae/Models/Turno.cs
+++ Medisysnae/Medisysnae/Models/Turno.cs
@@ -11,11 +11,14 @@ namespace Medisysnae.Models
         public int CodPaciente { get; set; }
         public string Comentario { get; set; }
         public int CodMedico { get; set; }
+        public string LugarAtencion { get; set; }
+
+        public enum Estado { Otorgado, Reservado, Cancelado, Atendido, Bloqueado };
+        public enum Accion { Otorgar, Reservar, Cancelar, Atender, Bloquear };
 
         public DateTime FechaTurno { get; set; }
-        public enum Estado { Otorgado, Reservado, Cancelado, Atendido, Bloqueado};
         public TimeSpan HoraComienzo { get; set; }
-        public DateTime? FechaHoraCancelacion { get; set; }
+        public TimeSpan HoraFin { get; set; }
 
 
     }
---
+++ Medisysnae/Medisysnae/Pages/Turnos/Create.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Medisysnae.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Medisysnae.Pages.Turnos
+{
+    public class CreateModel : PageModel
+    {
+        [BindProperty]
+        public Turno TurnoActual { get; set; }
+        [BindProperty]
+        public string EstadoActual { get; set; }
+
+        public SelectList EstadosList { get; set; }
+        public SelectList PacientesList { get; set; }
+        public List<Paciente> Pacientes { get; set; }
+
+        private readonly Medisysnae.Data.MedisysnaeContext _context;
+
+        public CreateModel(Medisysnae.Data.MedisysnaeContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            CargarPacientes();
+            CargarEstados();
+
+            return Page();
+        }
+
+        private void CargarEstados()
+        {
+            List<string> Estados = new List<string>();
+            Estados.Add(Turno.Accion.Bloquear.ToString());
+            Estados.Add(Turno.Accion.Cancelar.ToString());
+            Estados.Add(Turno.Accion.Reservar.ToString());
+
+            EstadosList = new SelectList(Estados);
+        }
+
+        private void CargarPacientes()
+        {
+            Pacientes = _context.Paciente.OrderBy(i => i.ApellidoNombre)
+                .ToList();
+            PacientesList = new SelectList(Pacientes, "ID", "ApellidoNombre", null);
+           
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
